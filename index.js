7f4523ccd4a33a2522f0b3223ff725acf952eb84
porterwg <porterwg@byu.edu>
2024-12-05 11:40:49 -0700

    fixed state names

--- index.js
+++ index.js
@@ -119,11 +119,11 @@ app.get('/donationCompletion/', (req, res) => {
 app.get('/volunteerSignup/', (req, res) => {
 
   const states = [
-    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
-    "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
-    "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-    "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-    "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+    "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+    "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
 ];
 
   res.render('volunteerSignup', {states} )
@@ -157,7 +157,7 @@ app.post('/volunteerSignup/', (req, res) => {
       volunteer_phone: volunteer_phone,
       volunteer_address: volunteer_address.toLowerCase(),
       volunteer_city: volunteer_city.toLowerCase(),
-      volunteer_state: volunteer_state,
+      volunteer_state: volunteer_state.toLowerCase(),
       volunteer_zip: volunteer_zip,
       volunteer_referral: volunteer_referral,
       volunteer_willing_hours: volunteer_willing_hours,
@@ -328,7 +328,6 @@ app.post('/submit-event-request', (req, res) => {
     requestee_phone: contact_phone,
     preferred_contact_method: contact_preferred_contact,
   } = req.body;
-
 
   // Sanitize optional fields
   const sanitizedBackupDate2 = event_backup_date_2 || null;
