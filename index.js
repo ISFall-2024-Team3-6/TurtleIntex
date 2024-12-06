cec7ae13f5649df423e726ab48dc6a4f5fe04ca0
Davis Larson <davislarson01@gmail.com>
2024-12-06 08:43:31 -0700

    get ready to zip

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const knex = require("knex") ({
   connection : {
       host : process.env.RDS_HOSTNAME || "awseb-e-ivmpumma37-stack-awsebrdsdatabase-upatlbu0rklt.cp6ss68iifrg.us-east-1.rds.amazonaws.com",
       user : process.env.RDS_USERNAME || "intexUser",
-      password : process.env.RDS_PASSWORD || "Thisisforintex1!",
+      password : process.env.RDS_PASSWORD || "",
       database : process.env.RDS_DB_NAME || "ebdb",
       port : process.env.RDS_PORT ||  5432,
       ssl : process.env.DB_SSL ? {rejectUnauthorized : false} : {rejectUnauthorized : false}
